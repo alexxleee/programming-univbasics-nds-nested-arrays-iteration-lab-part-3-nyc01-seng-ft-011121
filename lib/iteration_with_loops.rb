6d8a3b2b72140f4abad37b192af859de452d41db
Jung Ho Lee <alexxleee@gmail.com>
2021-01-02 23:52:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.length do
     column_index = 0
     while column_index < src[row_index] do
-      if 
+      if src[row_index][column_index].instance_of
   
   
 end
